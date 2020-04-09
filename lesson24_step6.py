a0c9ceee1cdb4b8450e873c4db463fbd6a359fc3
testserg <testserg@test.local>
2020-04-09 20:32:34 +0300

    homework from previous module

---
+++ lesson24_step6.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+import time
+try:
+
+    link = "http://suninjuly.github.io/cats.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    button = browser.find_element_by_id("button")
+
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
