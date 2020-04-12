f95a58e3626dee81f4189159cad831969879e55c
testserg <testserg@test.local>
2020-04-12 21:21:33 +0300

    add check visibility of basket

--- test_items.py
+++ test_items.py
@@ -1,8 +1,14 @@
 import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_find_button_add_to_basket(browser):
-    browser.implicitly_wait(2)
     browser.get(link)
     #time.sleep(30)
-    assert browser.find_element_by_css_selector(".btn-add-to-basket")
+    button = WebDriverWait(browser, 20).until(EC.visibility_of_element_located((
+        By.CSS_SELECTOR, '.btn-add-to-basket'))
+    )
+    assert button.is_displayed(), "button not displayed"
